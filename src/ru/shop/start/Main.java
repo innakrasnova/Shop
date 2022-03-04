4d493083912f09ff19a057aea0e9416f25802171
Nanka <moknat88@mail.ru>
2022-03-04 13:46:49 +0300

    add new file and new coment

--- src/ru/shop/start/Main.java
+++ src/ru/shop/start/Main.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 
 public class Main {
 
+    // Sysem.out.println(homework #11);
+
             public static void main(String[] args) {
                 doShoppingProcess();
             }
